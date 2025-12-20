c83a8e8b5c5dfb9d3893e9f42e04eaad2b2907e5
Luka <mikautadzeluka@gmail.com>
2025-12-20 14:38:22 -0800

    completed conterization tasks

--- TrainingMicroservice/src/main/java/com/example/TrainingMicroservice/config/SecurityConfig.java
+++ TrainingMicroservice/src/main/java/com/example/TrainingMicroservice/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/actuator/health", "/actuator/health/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .csrf(AbstractHttpConfigurer::disable)
